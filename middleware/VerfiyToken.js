6bb811ded1aae6f34171b24c6ef559a6acccd35a
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-10-29 18:52:05 +0300

    check role if its admin

--- middleware/VerfiyToken.js
+++ middleware/VerfiyToken.js
@@ -5,6 +5,8 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
       console.log("decoded", decoded);
+      if (decoded.role !== "admin")
+        return res.status(401).json({ msg: "you are not authorized to access this route" });
       req.userData = decoded;
       if (err) {
         return res.status(401).json({ msg: "token is not valid: ", err });
